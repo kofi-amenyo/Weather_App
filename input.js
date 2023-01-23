fda5d9073a92c495ce5c4e17058a3ca6b13af8e3
kofi-amenyo <mkamenyo@gmail.com>
2023-01-23 08:03:20 +0000

    Adding API call and JS functionality

---
+++ input.js
@@ -0,0 +1,15 @@
+//Adding event listener to submit form for weather data 
+
+const form = document.querySelector(".top-banner form");
+    form.addEventListener ("submit", e => {
+        e.preventDefault();
+        const inputVal = input.value;
+    })
+
+
+//Performing Ajax request using Personal API key
+
+const apiKey = "2f9271df87b5da3ebe220fa3b68bfeed";
+const inputVal = input.value;
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q={inputVal}&appid={apiKey}&units=metric"
